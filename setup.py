b80da19fa48a0cf9cb40e218f09d9269a9db93a6
harry lin <harry11815@gmail.com>
2020-06-29 14:37:00 +0800

    My bank web, first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("read.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="My bank", 
+    version="0.0.1",
+    author="harry lin",
+    author_email="harry11815@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Zhan-HongLin/mybank",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
